f00b0fc3eb44c1ffcf982fc82b2385ca1eeb0b44
galicians <galicians@gmail.com>
2014-11-04 22:30:31 +0000

    add new question tested

--- spec/sessionSpec.js
+++ spec/sessionSpec.js
@@ -12,6 +12,14 @@ describe("A study session", function() {
 		expect(session).toBeDefined()
 	})
 
+	it("needs questions to be defined", function() {
+		expect(session.questions).toBeDefined()
+	})
+
+	it("needs roundAnswers to be defined", function() {
+		expect(session.roundAnswers).toBeDefined()
+	})
+
 	it("should accept words", function() {
 		expect(session.words).toBe(words)
 	})
@@ -21,5 +29,14 @@ describe("A study session", function() {
 		expect(session._generateRandNumber()).toBeGreaterThan(-1)
 	})
 
+	it("knows when a question is repeated", function() {
+		session.questions.push(1)
+		expect(session._isQuestionRepeated(1)).toBeTruthy()
+	})
+
+	it("generates a new question from words", function() {
+		session.generateNewQuestion()
+		expect(session.questions.length).toEqual(1)
+	})
 
 })
--- src/session.js
+++ src/session.js
@@ -4,9 +4,22 @@ function Session() {
   this.roundAnswers = []
   this.learning
   this.mTongue
+  this.user
+  this.score
 }
 
 Session.prototype._generateRandNumber =  function() {
-  var total = this.words.length - 1
-  return Math.floor(Math.random() * total)
+  var totalWords = this.words.length - 1
+  return Math.floor(Math.random() * totalWords)
+}
+
+Session.prototype.generateNewQuestion = function() {
+  var randNumber = this._generateRandNumber() 
+  while(this._isQuestionRepeated(randNumber)){ randNumber = this._generateRandNumber() }
+  this.questions.push(randNumber) 
+  // this._isQuestionRepeated(randNumber) ? this.questions.push(randNumber) : this.generateNewQuestion()
+}
+
+Session.prototype._isQuestionRepeated = function(num) {
+  return this.questions.indexOf(num) !== -1
 }
