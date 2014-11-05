03f76c5a4604ea722b1a7ba94473fc8698c4e565
galicians <galicians@gmail.com>
2014-11-05 00:56:17 +0000

    all tests passing

--- spec/sessionSpec.js
+++ spec/sessionSpec.js
@@ -6,6 +6,7 @@ describe("A study session", function() {
 	beforeEach(function() {
 		session = new Session
 		session.words = words
+		session.learning = 'es'
 	})
 
 	it("should be defined", function() {
@@ -38,5 +39,38 @@ describe("A study session", function() {
 		session.generateNewQuestion()
 		expect(session.questions.length).toEqual(1)
 	})
+
+	it("can access to the current question", function() {
+		session.generateNewQuestion()
+		expect(session.words).toContain(session.currentQuestion())
+	})
+
+	it("knows what is the correct response to the current question", function() {
+		session.generateNewQuestion()
+		expect(session.correctResponse()).toEqual(session.currentQuestion()['es'])
+	})
+
+	it("knows when a response is repeated in a round", function(){
+		session.roundAnswers.push(words[0]['es'])
+		expect(session.isResponseRepeated(0)).toBeTruthy()
+	})
+
+	it("generates a fake answer", function() {
+		session.generateNewQuestion()
+		expect(session.generateFakeAnswer()).not.toEqual(session.correctResponse())
+	})
+
+	it("generates as many answers as you need per round", function() {
+		session.generateNewQuestion()
+		session.generateAnswers(2)
+		expect(session.roundAnswers.length).toEqual(3)
+	})
+
+	it("includes the correct response between the answers generated", function() {
+		session.generateNewQuestion()
+		session.generateAnswers(4)
+		expect(session.roundAnswers).toContain(session.correctResponse())
+	})
+
 
 })
--- src/session.js
+++ src/session.js
@@ -15,7 +15,7 @@ Session.prototype._generateRandNumber =  function() {
 
 Session.prototype.generateNewQuestion = function() {
   var randNumber = this._generateRandNumber() 
-  while(this._isQuestionRepeated(randNumber)){ randNumber = this._generateRandNumber() }
+  while ( this._isQuestionRepeated(randNumber) ) { randNumber = this._generateRandNumber() }
   this.questions.push(randNumber) 
   // this._isQuestionRepeated(randNumber) ? this.questions.push(randNumber) : this.generateNewQuestion()
 }
@@ -23,3 +23,42 @@ Session.prototype.generateNewQuestion = function() {
 Session.prototype._isQuestionRepeated = function(num) {
   return this.questions.indexOf(num) !== -1
 }
+
+Session.prototype.currentQuestion = function() {
+  var index = this.questions.length - 1
+  return words[index]
+}
+
+Session.prototype.correctResponse = function() {
+  return this.currentQuestion()[this.learning]
+}
+
+Session.prototype.isResponseRepeated = function(num) {
+  return this.roundAnswers.indexOf(this.words[num][this.learning]) !== -1
+}
+
+Session.prototype.generateFakeAnswer = function() {
+  var randNumber = this._generateRandNumber()
+  while ( this.isResponseRepeated(randNumber) ) { randNumber = this._generateRandNumber() }
+  return this.words[randNumber][this.learning]
+}
+
+Session.prototype.generateAnswers = function(num) {
+   this.roundAnswers.push(this.correctResponse())
+  for(var i = 0; i < num; i++){ this.roundAnswers.push(this.generateFakeAnswer()) }
+}
+
+Session.prototype.shuffleAnswers = function(){
+    this.roundAnswers = shuffle(this.roundAnswers)
+}
+//Array.prototype ??
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) { 
+        var j = Math.floor(Math.random() * (i + 1))
+        var temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+    return array
+}
+
